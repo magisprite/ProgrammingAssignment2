1220932c32892f5051152759c6a4c45b60e0960b
Matthew Straus <Matt@Matthews-MBP.fios-router.home>
2015-07-26 13:28:09 -0400

    updated hw file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a matrix object capable of caching an inverse and returning it. Also creates the function to calculate the inverse
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+library('MASS')
 
+## creates the matrix object capable of caching an inverse
+makeCacheMatrix <- function(x = matrix()){
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse<- function () i
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
+ ##solves the inverse of a matrix and stores it in the makeCacheMatrix object
 
-## Write a short comment describing this function
+cacheSolve <- function(x,...){
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!isnull(i)){ 
+    ## if the matrix's solution is not empty, returns the matrix's solution 
+    mesage("getting cached data")
+    return(i)
+  }
+  data <-x$get()
+  if(nrow(data) ==2 && ncol(data)== 2){
+    xinverse = solve(data) ## if the matrix is a 2x2 matrix it uses solve
+  }
+  else{ xinverse = ginv(data)} 
+  ## if the matrix is not a 2x2 matrix it will use the built in ginv function
+  x$setinverse(xinverse) ##stores the inverse matrix in the x object
+  xinverse ##returns the inverse matrix 
 }
